e977663a42467ca48c39ba0c2e7d4bbfba40b850
tam40842 <pqtam40842@gmail.com>
2023-04-07 00:05:23 +0700

    update

--- client/src/pages/MediaSearch.jsx
+++ client/src/pages/MediaSearch.jsx
@@ -5,7 +5,48 @@ import { toast } from "react-toastify";
 import mediaApi from "../api/modules/media.api";
 import MediaGrid from "../components/common/MediaGrid";
 import uiConfigs from "../configs/ui.configs";
+
+const mediaTypes = ["movie", "tv", "people"];
+let timer;
+const timeout = 500;
+
 const MediaSearch = () => {
+  const [query, setQuery] = useState("");
+  const [onSearch, setOnSearch] = useState(false);
+  const [mediaType, setMediaType] = useState(mediaTypes[0]);
+  const [medias, setMedias] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const search = useCallback(async () => {
+    setOnSearch(true);
+    const { response, err } = await mediaApi.search({
+      mediaType,
+      query,
+      page,
+    });
+
+    setOnSearch(false);
+
+    if (err) toast.error(err.message);
+    if (response) {
+      if (page > 1) setMedias((m) => [...m, ...response.results]);
+      else setMedias([...response.results]);
+    }
+  }, [mediaType, query, page]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (query.trim().length === 0) {
+      setMedias([]);
+      setPage(1);
+    } else search();
+  }, [search, query, mediaType]);
+
+  useEffect(() => {
+    setMedias([]);
+    setPage(1);
+  }, [mediaType]);
+
   return <div>MediaSearch</div>;
 };
 
